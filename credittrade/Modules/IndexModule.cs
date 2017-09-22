31c5d9d25a4a4fd7f85283bba8faf733f8390d93
Eugeny Lutsenko <st.lui@live.ru>
2017-09-22 15:29:58 +0700

    Новый вид

--- credittrade/Modules/IndexModule.cs
+++ credittrade/Modules/IndexModule.cs
@@ -230,6 +230,50 @@ namespace credittrade.Modules
 					return Response.FromByteArray(ms.GetBuffer(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 				}
 			};
+			Get["/report_goods_leftovers"] = p =>
+			{
+				using (UnitOfWork unitOfWork = (UnitOfWork)Context.Items["unitofwork"])
+				{
+					user currentUser = ((Bootstrapper.User)Context.CurrentUser).DbUser;
+					warehouse warehouse = currentUser.warehouse;
+					model.Post = warehouse.name;
+					model.Post_id = warehouse.id;
+					var today = DateTime.Today;
+					string StartDate = new DateTime(today.Year, today.Month, 1).ToString("d", CultureInfo.InvariantCulture);
+					string FinishDate = new DateTime(today.Year, today.Month, DateTime.DaysInMonth(today.Year, today.Month)).ToString("d", CultureInfo.InvariantCulture);
+					model.StartDate = StartDate;
+					model.FinishDate = FinishDate;
+					model.Layout = "layout.cshtml";
+
+				}
+				return View["report_goods_leftovers", model];
+			};
+
+			Post["/report_goods_leftovers"] = param =>
+			{
+				using (UnitOfWork unitOfWork = (UnitOfWork)Context.Items["unitofwork"])
+				{
+					DateTime start = DateTime.Parse(Request.Form["date_start"]);
+					DateTime finish = DateTime.Parse(Request.Form["date_finish"]);
+					int warehouseId = int.Parse(Request.Form["post"]);
+					MemoryStream ms = new MemoryStream();
+					warehouse warehouse = unitOfWork.Warehouses.GetWithBuyers(warehouseId);
+
+					List<buyer> buyersPost = warehouse.buyers.ToList();
+					var buyerIds = buyersPost.Select(x => x.id).ToList();
+					IList<request> reqs = unitOfWork.Requests.GetPaidRequestsWithRowsByDate(buyerIds, start, finish);
+					InOutReportModel reportModel = new InOutReportModel();
+					reportModel.Start = Request.Form["date_start"];
+					reportModel.Finish = Request.Form["date_finish"];
+					reportModel.RequestsCurrent = reqs;
+					ms = Utils.GenReportGoods(reportModel, " оплаченным");
+					//return Response.FromStream(ms, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+					//return Response.FromByteArray(ms.GetBuffer(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+
+					return Response.FromByteArray(ms.GetBuffer(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+				}
+			};
 		}
 	}
 }
